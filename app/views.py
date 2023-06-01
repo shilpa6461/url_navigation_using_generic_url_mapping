d960e11644ffa90f2c38ad483e7f1914e48b3311
mshilpa54321 <mshilpa54321@gmail.com>
2023-06-01 13:40:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def shilpa(request):
+    return render(request,'shilpa.html')
+def chandu(request):
+    return render(request,'chandu.html')
